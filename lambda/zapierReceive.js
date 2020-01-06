69024e75e2c4631b667e0191557974a3f8a892a6
Brian Rinaldi <brian.rinaldi@gmail.com>
2020-01-06 10:38:00 -0500

    Update zapierReceive.js

--- lambda/zapierReceive.js
+++ lambda/zapierReceive.js
@@ -5,7 +5,7 @@ const fs = require('fs'),
 
 exports.handler = async (event, context, callback) => {
   try {
-    const octokit = new Octokit({auth:'e6cc755319b23616f111a7826c083bb29c2547f0'});
+    const octokit = new Octokit({auth:process.env.GITHUB_TOKEN});
     if(!event.body) {
       return { 
           statusCode: 500, 
@@ -28,17 +28,39 @@ exports.handler = async (event, context, callback) => {
             body: 'link parameter required' 
         };
     }
-
-    octokit.repos.getContents({
+    let path = 'links.json';
+    return octokit.repos.getContents({
       owner,
       repo,
-      'links.json'
-    })
+      path
+    }).then(res => {
+      console.log(res);
+      let buff = Buffer.from(res.data.content, 'base64');
+      let linksRaw = buff.toString('utf-8');
+      let linksJSON = JSON.parse(linksRaw);
+      let message = 'Updated links';
+      let content = '';
+      let sha = res.data.sha;
+      linksJSON.links.push(newItem);
+      linksRaw = JSON.stringify(linksJSON);
+      buff = Buffer.from(linksRaw);
+      content = buff.toString('base64');
+      return octokit.repos.createOrUpdateFile({
+        owner,
+        repo,
+        path,
+        message,
+        content,
+        sha
+      }).then(res => {
+        return {
+          statusCode:200, 
+          body: '{"success":"true"}'
+        }
+      })
+    });
 
-    return {
-      statusCode:200, 
-      body: '{"success":"true"}'
-    }
+    
   } catch (err) {[]
     return { statusCode: 500, body: err.toString() };
   }
